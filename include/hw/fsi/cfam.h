bd8b518e8670db2243bb2dfc7e485f4509768ffc
Mikail-Sadic <mikail.sadic@ibm.com>
2026-06-25 12:06:59 -0500

    fsi/cfam: Rework CFAM model for AST2700
    Update the CFAM model to match the behavior expected by newer Linux kernels.
    
    Replace the POWER9 chip-id and LBUS/scratchpad layout with a CFAM-S
    config table and mailbox scratch registers, allowing the FSI
    subsystem to enumerate and initialize the device correctly.
    
    Signed-off-by: Mikail Sadic <mikail.sadic@ibm.com>

--- include/hw/fsi/cfam.h
+++ include/hw/fsi/cfam.h
@@ -1,34 +1,26 @@
 /*
- * SPDX-License-Identifier: GPL-2.0-or-later
+ * IBM Common FRU Access Macro (CFAM)
+ *
  * Copyright (C) 2024 IBM Corp.
  *
- * IBM Common FRU Access Macro
+ * SPDX-License-Identifier: GPL-2.0-or-later
  */
 #ifndef FSI_CFAM_H
 #define FSI_CFAM_H
 
 #include "system/memory.h"
-
 #include "hw/fsi/fsi.h"
-#include "hw/fsi/lbus.h"
 
 #define TYPE_FSI_CFAM "cfam"
 #define FSI_CFAM(obj) OBJECT_CHECK(FSICFAMState, (obj), TYPE_FSI_CFAM)
 
-/* P9-ism */
-#define CFAM_CONFIG_NR_REGS 0x28
+#define CFAM_MBOX_SCRATCH_NUM 5
 
 typedef struct FSICFAMState {
-    /* < private > */
     FSISlaveState parent;
 
-    /* CFAM config address space */
-    MemoryRegion config_iomem;
-
     MemoryRegion mr;
-
-    FSILBus lbus;
-    FSIScratchPad scratchpad;
+    uint32_t mbox_scratch[CFAM_MBOX_SCRATCH_NUM];
 } FSICFAMState;
 
 #endif /* FSI_CFAM_H */
