62137f5368196f2e22ea6fd859da497dae310685
Mikail-Sadic <mikail.sadic@ibm.com>
2026-07-02 10:32:55 -0500

    ufs/aspeed: Add AST2700 UFS host controller
    Add an emulation of the AST2700 UFS host controller
    (aspeed,ufshc-m31-16nm) and wire it into the AST2700 SoC.
    
    Implement the functionality needed by U-Boot and Linux to enumerate the
    device and access a BlockBackend using the UFS protocol, allowing the
    AST2700 machine to boot from a UFS image.
    
    Signed-off-by: Mikail Sadic <mikail.sadic@ibm.com>

--- include/hw/arm/aspeed_soc.h
+++ include/hw/arm/aspeed_soc.h
@@ -35,6 +35,7 @@
 #include "hw/gpio/aspeed_gpio.h"
 #include "hw/gpio/aspeed_sgpio.h"
 #include "hw/sd/aspeed_sdhci.h"
+#include "hw/ufs/aspeed_ufs.h"
 #include "hw/usb/hcd-ehci.h"
 #include "qom/object.h"
 #include "hw/misc/aspeed_lpc.h"
@@ -118,6 +119,7 @@ struct AspeedSoCState {
     AspeedAPB2OPBState fsi[2];
     AspeedLTPIState ltpi_ctrl[ASPEED_IOEXP_NUM];
     AspeedAST1700SoCState ioexp[ASPEED_IOEXP_NUM];
+    AspeedUFSState ufs;
 };
 
 #define TYPE_ASPEED_SOC "aspeed-soc"
@@ -295,6 +297,7 @@ enum {
     ASPEED_DEV_IOEXP1_INTCIO,
     ASPEED_DEV_IOEXP0_I3C,
     ASPEED_DEV_IOEXP1_I3C,
+    ASPEED_DEV_UFS,
 };
 
 const char *aspeed_soc_cpu_type(const char * const *valid_cpu_types);
